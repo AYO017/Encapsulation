317b71db61ae85144bd7f5c2ad110d1af9abfbf4
MR Jad <Ayo@192.168.0.124>
2018-12-28 22:10:19 +0000

    How to Encapsulate variables in java

---
+++ Encapsulation_Test/src/Encapsulation_demo.java
@@ -0,0 +1,33 @@
+
+public class Encapsulation_demo {
+
+	private int age;
+	private String firstName;
+	private String secondName;
+
+	// Getter and Setter methods
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public String getsecondName() {
+		return secondName;
+	}
+
+	public void setAge(int From_User) {
+		age = From_User;
+	}
+
+	public void setfirstName(String From_User) {
+		firstName = From_User;
+	}
+
+	public void setsecondName(String From_User) {
+		secondName = From_User;
+	}
+}
---
+++ Encapsulation_Test/src/run.java
@@ -0,0 +1,17 @@
+
+public class run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Encapsulation_demo en = new Encapsulation_demo();
+	en.setAge(24);
+	en.setfirstName("AYO");
+	en.setsecondName("AKIN");
+	
+	System.out.println("Age:" +en.getAge());
+	System.out.println("First Name:"+en.getfirstName());
+	System.out.println("Second Name:"+en.getsecondName());
+	}
+
+}
